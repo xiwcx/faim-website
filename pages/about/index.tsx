42736b2e74a5f78a7b9d208d86786e14b825af37
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2022-06-13 17:30:22 -0700

    add about component

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,20 +1,11 @@
 import type { NextPage } from "next";
+import { About } from "../../components/about/about";
 import LayoutDefault from "../../components/layout-default/layout-default";
 
 const Store: NextPage = () => {
   return (
     <LayoutDefault>
-      <article>
-        <p>A hardcore band from Denver, Colorado.</p>
-
-        <ul>
-          <li>Chris — Guitar</li>
-          <li>Chris — Guitar</li>
-          <li>Kat — Vocals</li>
-          <li>Nick — Drums</li>
-          <li>Welch — Bass</li>
-        </ul>
-      </article>
+      <About />
     </LayoutDefault>
   );
 };
